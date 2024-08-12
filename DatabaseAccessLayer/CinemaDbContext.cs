1ced3e3121aac31513d568fdd18c50cec60617cc
Beelz76 <invalid-petrov@yandex.ru>
2024-08-12 17:27:11 +0300

    update CinemaDbContext

--- DatabaseAccessLayer/CinemaDbContext.cs
+++ DatabaseAccessLayer/CinemaDbContext.cs
@@ -7,59 +7,88 @@ namespace DatabaseAccessLayer
     {
         public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
         {
-
         }
-
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<User>().HasKey(x => x.UserId);
-            builder.Entity<User>().HasMany(x => x.Tickets).WithOne(x => x.User).HasForeignKey("UserId");
+            builder.Entity<User>(entity => 
+            {
+                entity.HasKey(x => x.UserId);
+                entity.HasMany(x => x.Tickets).WithOne(x => x.User).HasForeignKey("UserId"); 
+            });
+            
+            builder.Entity<Movie>(entity =>
+            {
+                entity.HasKey(x => x.MovieId);
+                entity.HasMany(x => x.Screenings).WithOne(x => x.Movie).HasForeignKey("MovieId");
+            });
 
-            builder.Entity<Movie>().HasKey(x => x.MovieId);
-            builder.Entity<Movie>().HasMany(x => x.Screenings).WithOne(x => x.Movie).HasForeignKey("MovieId");
+            builder.Entity<Director>(entity =>
+            {
+                entity.HasKey(x => x.DirectorId);
+                entity.HasMany(x => x.Movies).WithMany(x => x.Directors)
+                    .UsingEntity("MovieDirector",
+                        l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
+                        r => r.HasOne(typeof(Director)).WithMany().HasForeignKey("DirectorId").HasPrincipalKey(nameof(Director.DirectorId)),
+                        j => j.HasKey("DirectorId", "MovieId"));
+            });
 
-            builder.Entity<Director>().HasKey(x => x.DirectorId);
-            builder.Entity<Director>().HasMany(x => x.Movies).WithMany(x => x.Directors)
-                .UsingEntity("MovieDirector",
-                    l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
-                    r => r.HasOne(typeof(Director)).WithMany().HasForeignKey("DirectorId").HasPrincipalKey(nameof(Director.DirectorId)),
-                    j => j.HasKey("DirectorId", "MovieId"));
+            builder.Entity<Country>(entity =>
+            {
+                entity.HasKey(x => x.CountryId);
+                entity.HasMany(x => x.Movies).WithMany(x => x.Countries)
+                    .UsingEntity("MovieCountry",
+                        l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
+                        r => r.HasOne(typeof(Country)).WithMany().HasForeignKey("CountryId").HasPrincipalKey(nameof(Country.CountryId)),
+                        j => j.HasKey("CountryId", "MovieId"));
+            });
+            
+            builder.Entity<Genre>(entity =>
+            {
+                entity.HasKey(x => x.GenreId);
+                entity.HasMany(x => x.Movies).WithMany(x => x.Genres)
+                    .UsingEntity("MovieGenre",
+                        l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
+                        r => r.HasOne(typeof(Genre)).WithMany().HasForeignKey("GenreId").HasPrincipalKey(nameof(Genre.GenreId)),
+                        j => j.HasKey("GenreId", "MovieId"));
+            });
+            
+            builder.Entity<Seat>(entity =>
+            {
+                entity.HasKey(x => x.SeatId);
+                entity.HasMany(x => x.Tickets).WithOne(x => x.Seat).HasForeignKey("SeatId");
+                entity.HasOne(x => x.Hall).WithMany(x => x.Seats).HasForeignKey("HallId");
+            });
 
-            builder.Entity<Country>().HasKey(x => x.CountryId);
-            builder.Entity<Country>().HasMany(x => x.Movies).WithMany(x => x.Countries)
-                .UsingEntity("MovieCountry",
-                    l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
-                    r => r.HasOne(typeof(Country)).WithMany().HasForeignKey("CountryId").HasPrincipalKey(nameof(Country.CountryId)),
-                    j => j.HasKey("CountryId", "MovieId"));
+            builder.Entity<Hall>(entity =>
+            {
+                entity.HasKey(x => x.HallId);
+                entity.HasMany(x => x.Screenings).WithOne(x => x.Hall).HasForeignKey("HallId");
+                entity.HasMany(x => x.Seats).WithOne(x => x.Hall).HasForeignKey("HallId");
+            });
+            
+            builder.Entity<Screening>(entity =>
+            {
+                entity.HasKey(x => x.ScreeningId);
+                entity.HasMany(x => x.Tickets).WithOne(x => x.Screening).HasForeignKey("ScreeningId");
+                entity.HasOne(x => x.ScreeningPrice).WithMany(x => x.Screenings).HasForeignKey("ScreeningPriceId");
+                entity.HasOne(x => x.Movie).WithMany(x => x.Screenings).HasForeignKey("MovieId");
+                entity.HasOne(x => x.Hall).WithMany(x => x.Screenings).HasForeignKey("HallId");
+            });
 
-            builder.Entity<Genre>().HasKey(x => x.GenreId);
-            builder.Entity<Genre>().HasMany(x => x.Movies).WithMany(x => x.Genres)
-                .UsingEntity("MovieGenre",
-                l => l.HasOne(typeof(Movie)).WithMany().HasForeignKey("MovieId").HasPrincipalKey(nameof(Movie.MovieId)),
-                r => r.HasOne(typeof(Genre)).WithMany().HasForeignKey("GenreId").HasPrincipalKey(nameof(Genre.GenreId)),
-                j => j.HasKey("GenreId", "MovieId"));
-
-            builder.Entity<Seat>().HasKey(x => x.SeatId);
-            builder.Entity<Seat>().HasMany(x => x.Tickets).WithOne(x => x.Seat).HasForeignKey("SeatId");
-            builder.Entity<Seat>().HasOne(x => x.Hall).WithMany(x => x.Seats).HasForeignKey("HallId");
-
-            builder.Entity<Hall>().HasKey(x => x.HallId);
-            builder.Entity<Hall>().HasMany(x => x.Screenings).WithOne(x => x.Hall).HasForeignKey("HallId");
-            builder.Entity<Hall>().HasMany(x => x.Seats).WithOne(x => x.Hall).HasForeignKey("HallId");
-
-            builder.Entity<Screening>().HasKey(x => x.ScreeningId);
-            builder.Entity<Screening>().HasMany(x => x.Tickets).WithOne(x => x.Screening).HasForeignKey("ScreeningId");
-            builder.Entity<Screening>().HasOne(x => x.ScreeningPrice).WithMany(x => x.Screenings).HasForeignKey("ScreeningPriceId");
-            builder.Entity<Screening>().HasOne(x => x.Movie).WithMany(x => x.Screenings).HasForeignKey("MovieId");
-            builder.Entity<Screening>().HasOne(x => x.Hall).WithMany(x => x.Screenings).HasForeignKey("HallId");
-
-            builder.Entity<ScreeningPrice>().HasKey(x => x.ScreeningPriceId);
-            builder.Entity<ScreeningPrice>().HasMany(x => x.Screenings).WithOne(x => x.ScreeningPrice).HasForeignKey("ScreeningPriceId");
-
-            builder.Entity<Ticket>().HasKey(x => x.TicketId);
-            builder.Entity<Ticket>().HasOne(x => x.User).WithMany(x => x.Tickets).HasForeignKey("UserId");
-            builder.Entity<Ticket>().HasOne(x => x.Screening).WithMany(x => x.Tickets).HasForeignKey("ScreeningId");
-            builder.Entity<Ticket>().HasOne(x => x.Seat).WithMany(x => x.Tickets).HasForeignKey("SeatId");
+            builder.Entity<ScreeningPrice>(entity =>
+            {
+                entity.HasKey(x => x.ScreeningPriceId);
+                entity.HasMany(x => x.Screenings).WithOne(x => x.ScreeningPrice).HasForeignKey("ScreeningPriceId");
+            });
+            
+            builder.Entity<Ticket>(entity =>
+            {
+                entity.HasKey(x => x.TicketId);
+                entity.HasOne(x => x.User).WithMany(x => x.Tickets).HasForeignKey("UserId");
+                entity.HasOne(x => x.Screening).WithMany(x => x.Tickets).HasForeignKey("ScreeningId");
+                entity.HasOne(x => x.Seat).WithMany(x => x.Tickets).HasForeignKey("SeatId");
+            });
         }
     }
 }
